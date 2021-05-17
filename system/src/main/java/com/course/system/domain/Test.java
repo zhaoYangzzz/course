83e852c4705cae9b8f8fa5f4bd35563037708186
lzy7706 <1907876975@qq.com>
2021-05-17 15:17:08 +0800

    1.Mybatis使用示例：http://127.0.0.1:9001/system/test 更改mysql数据库版本与配置文件的驱动连接 2.增加三层结构

---
+++ system/src/main/java/com/course/system/domain/Test.java
@@ -0,0 +1,30 @@
+package com.course.system.domain;
+
+public class Test {
+    private String id;
+    private String name;
+
+    public Test() {
+    }
+
+    public Test(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

838d6bad688253d5b8e9ada4b25807516ffd72fb
lzy7706 <1907876975@qq.com>
2021-05-19 17:44:06 +0800

    1.system集成server模块

--- system/src/main/java/com/course/system/controller/TestController.java
+++ system/src/main/java/com/course/system/controller/TestController.java
@@ -1,7 +1,10 @@
 package com.course.system.controller;
 
-import com.course.system.domain.Test;
-import com.course.system.service.TestService;
+
+import com.course.server.domain.Test;
+import com.course.server.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +13,8 @@ import java.util.List;
 
 @RestController
 public class TestController {
-    @Resource
-    private TestService testService;
+   @Resource
+   private TestService testService;
 
     @RequestMapping("/test")
     public List<Test> test(){
